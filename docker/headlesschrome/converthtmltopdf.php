2bdff9d93e909769dce37550c7fb15c35e708add
sanototsuka <tots.2.105@gmail.com>
2025-02-20 20:05:03 +0900

    thinkingreed-inc#1218 ヘッドレスChromeからPDFをエクスポートできないバグの修正

--- docker/headlesschrome/converthtmltopdf.php
+++ docker/headlesschrome/converthtmltopdf.php
@@ -10,12 +10,12 @@ if ($filepath) {
     // 白紙事象の対応の為、PDFのサイズが10kb未満の場合は再度変換処理を行う。最大20回までとする。
     for ($i = 0; $i < $max_loop_count; $i++) {
         // htmlからPDF変換
-        exec("/usr/bin/google-chrome --headless --no-sandbox --disable-setuid-sandbox --disable-software-rasterizer --disable-gpu --allow-file-access-from-files --virtual-time-budget=9999999 --run-all-compositor-stages-before-draw --no-pdf-header-footer --print-to-pdf=" . $filepath . ".pdf" . " " . $filepath . ".html 2>&1", $output, $retval);
+        exec("/usr/bin/google-chrome --headless --no-sandbox --disable-setuid-sandbox --disable-software-rasterizer --disable-gpu --allow-file-access-from-files --virtual-time-budget=9999999 --run-all-compositor-stages-before-draw --disable-dev-shm-usage --no-pdf-header-footer --print-to-pdf=" . $filepath . ".pdf" . " " . $filepath . ".html 2>&1", $output, $retval);
         if ($retval != 0) {
             error_log(date("Y/m/d H:i:s") . "." . substr(explode(".", (microtime(true) . ""))[1], 0, 3) . ", output:" . __LINE__ . print_r($output, true) . ", retval:" . $retval . PHP_EOL, 3, $logFilename);
         }
         // ファイルサイズが10kbより大きい場合はループを抜ける。10kb以下の場合は再度処理を行う。
-        if (filesize($filepath . ".pdf") > 10240) {
+        if (file_exists($filepath) && filesize($filepath . ".pdf") > 10240) {
             break;
         }
         error_log(date("Y/m/d H:i:s") . "." . substr(explode(".", (microtime(true) . ""))[1], 0, 3) . ", " . __LINE__ . ", loopcount:" . ($i + 1) . PHP_EOL, 3, $logFilename);
